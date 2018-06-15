b237aac27314965c9012f82e1c154d15159166e5
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2018-06-15 18:58:37 +0200

    test: setup tests (#11)

--- src/getBatches.js
+++ src/getBatches.js
@@ -1,13 +1,13 @@
 const HF_BLOCKS = [2889020];
 
-function getBatches(startBlock, blockCount) {
+function getBatches(startBlock, blockCount, batchSize = 50) {
   const batches = [];
 
   let batch = [];
   for (let i = 0; i < blockCount; i++) {
     batch.push(startBlock + i);
 
-    if (batch.length === 50 || i === blockCount - 1) {
+    if (batch.length === batchSize || i === blockCount - 1) {
       batches.push(batch.filter(block => !HF_BLOCKS.includes(block)));
       batch = [];
     }
