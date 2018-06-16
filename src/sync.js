49c62ef350682e9e54d8b4003504c88fc5b563d1
Fabien <bonustrack@users.noreply.github.com>
2018-06-16 15:28:59 +0700

    Index account_create operation (#13)
    * Add account_create operation
    
    * Remove new_account_name op

--- src/sync.js
+++ src/sync.js
@@ -43,7 +43,7 @@ async function processBatch(txs) {
       case "pow2":
         await addUser(timestamp, payload.work[1].input.worker_account);
         break;
-      case "new_account_name":
+      case "account_create":
       case "account_create_with_delegation":
         await addUser(timestamp, payload.new_account_name);
         break;
