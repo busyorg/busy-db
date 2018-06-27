76c6a7c680641648644f4a767ee051f2b1bb3ba6
adcpm <fabien@bonustrack.co>
2018-06-27 17:06:25 +0700

    Increase timeout to 10sec

--- helpers/api.js
+++ helpers/api.js
@@ -3,6 +3,6 @@ const Client = require("lightrpc");
 
 Promise.promisifyAll(Client.prototype);
 const address = process.env.STEEMD_URL || "https://api.steemit.com";
-const client = new Client(address);
+const client = new Client(address, { timeout: 10000 });
 
 module.exports = client;
